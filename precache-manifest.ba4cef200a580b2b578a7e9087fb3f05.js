f29b2f2d7e9b558056d6627e10ea865faf037f51
salishop <37633640+salishop@users.noreply.github.com>
2019-06-15 00:11:48 +0300

    Updates

---
+++ precache-manifest.ba4cef200a580b2b578a7e9087fb3f05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357c27aac7fb97ced9a94f665defa50e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "ed415f4f7cd996905455",
+    "url": "/todo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/todo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ed415f4f7cd996905455",
+    "url": "/todo/static/js/main.1746482e.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
